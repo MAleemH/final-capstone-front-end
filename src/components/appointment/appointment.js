a2e8bd6f2ff709fb6857121f795bf57dc9f98800
Choice Osobor <choiceosobor@gmail.com>
2023-06-12 14:42:37 +0100

    update appointment booking with parameters

--- src/components/appointment/appointment.js
+++ src/components/appointment/appointment.js
@@ -5,3 +5,15 @@ import { createAppointment } from '../../redux/appointment/appointment';
 const BookAppointment = () => {
     const dispatch = useDispatch();
     const doctordata = useSelector((state) => state.doctorReducer);
+    const { userid } = JSON.parse(window.localStorage.getItem('BookDoctorUser'));
+    const userID = userid || 1;
+    const { payload } = doctordata;
+    const doctorID = payload?.doctor.id;
+    const [disease, setDisease] = useState();
+    const [city, setCity] = useState();
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+    const [message, setMessage] = useState();
+    const [error, setError] = useState();
+    const [success, setSuccess] = useState();
+    
