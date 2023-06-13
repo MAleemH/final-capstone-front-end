d9d44577702ebe7e3f27a4499b469a6366e01c57
Choice Osobor <choiceosobor@gmail.com>
2023-06-13 16:24:37 +0100

    create variable to accept two unique parameters

--- src/redux/appointment/appointment.js
+++ src/redux/appointment/appointment.js
@@ -1,3 +1,15 @@
 const CREATE_APPOINTMENT = 'CREATE_APPOINTMENT';
 
 export const createNewAppointment = async (userid, datareserve) => {
+  const {
+    disease, city, appointmentDate, appointmentTime, doctorID,
+  } = datareserve;
+  const UserId = `${userid}`;
+  const reservation = {
+    disease,
+    city,
+    appointmentDate,
+    appointmentTime,
+    user: UserId,
+    doctor: doctorID,
+  };
