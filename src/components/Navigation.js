f80481b310c2672496032f048a01c8c4e07406d4
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-06-14 02:20:56 -0700

    Effectively displayed nav responsiveness in 768px brekpoint

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import '../css/Navigation.css';
 import { Link, NavLink } from 'react-router-dom';
 import '../css/LoginPage.css';
@@ -13,96 +13,112 @@ import cancelImg from '../img/cancel.png';
 
 function Navigation() {
   const [isToggled, setIsToggled] = useState(true);
-  // onClick={() => setIsToggled(!isToggled)}
+  const [mobileView, setMobileView] = useState(false);
+
+  useLayoutEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    const handleMediaQueryChange = (event) => {
+      setMobileView(event.matches);
+    };
+
+    handleMediaQueryChange(mediaQuery); // Initial check
+
+    const mediaQueryListener = (event) => handleMediaQueryChange(event);
+    mediaQuery.addEventListener('change', mediaQueryListener);
+
+    return () => {
+      mediaQuery.removeEventListener('change', mediaQueryListener);
+    };
+  }, []);
+
   return (
     <main>
-
-      <section className="home_nav_section_menu">
-        <header className={`home_logo ${isToggled ? 'show_logo' : 'hide_logo'}`}>
-          <button onClick={() => setIsToggled(!isToggled)} type="button" aria-label="button">
-            <img src={menuImg} alt="" />
-          </button>
-        </header>
-
-        <header className={`home_logo ${isToggled ? 'hide_logo' : 'show_logo'}`}>
-          <button onClick={() => setIsToggled(!isToggled)} type="button" aria-label="button">
-            <img src={cancelImg} alt="" />
-          </button>
-        </header>
-      </section>
-
       <section className="home_nav_section">
-        <header className="home_logo hide_logo">
+        {mobileView && (
+        <>
+          <header className="home_logo">
+            <button onClick={() => setIsToggled(!isToggled)} type="button" aria-label="button">
+              <img src={menuImg} alt="" />
+            </button>
+          </header>
+
+          <header className="home_logo">
+            <button onClick={() => setIsToggled(!isToggled)} type="button" aria-label="button">
+              <img src={cancelImg} alt="" />
+            </button>
+          </header>
+        </>
+        )}
+
+        {!mobileView && (
+        <header className="home_logo">
           <figure>
             <Link to="/homepage" className="td_none">
               <img src={logoImg} alt="" />
             </Link>
           </figure>
         </header>
-
-        {isToggled ? '' : (
-          <nav className="list_style_none home_nav">
-            <li>
-              {' '}
-              <NavLink to="/homepage" activeClassName="active" className="td_none">HOME</NavLink>
-            </li>
-            <li>
-              {' '}
-              <NavLink to="/" activeClassName="active" className="td_none">APPOINTMENTS</NavLink>
-            </li>
-            <li>
-              {' '}
-              <NavLink to="/" activeClassName="active" className="td_none">NEW APPOINTMENT</NavLink>
-            </li>
-            <li>
-              {' '}
-              <NavLink to="/newtherapist" activeClassName="active" className="td_none">NEW THERAPIST</NavLink>
-            </li>
-            <li>
-              {' '}
-              <NavLink to="/deletetherapist" className="td_none" activeClassName="active">DELETE THERAPIST</NavLink>
-            </li>
-            <li>
-              <button className="logout_btn" type="button">LOGOUT</button>
-            </li>
-          </nav>
         )}
 
-        {isToggled ? '' : (
-          <footer className="home_nav_footer">
+        <nav className="list_style_none home_nav">
+          <li>
+            {' '}
+            <NavLink to="/homepage" activeClassName="active" className="td_none">HOME</NavLink>
+          </li>
+          <li>
+            {' '}
+            <NavLink to="/" activeClassName="active" className="td_none">APPOINTMENTS</NavLink>
+          </li>
+          <li>
+            {' '}
+            <NavLink to="/" activeClassName="active" className="td_none">NEW APPOINTMENT</NavLink>
+          </li>
+          <li>
+            {' '}
+            <NavLink to="/newtherapist" activeClassName="active" className="td_none">NEW THERAPIST</NavLink>
+          </li>
+          <li>
+            {' '}
+            <NavLink to="/deletetherapist" className="td_none" activeClassName="active">DELETE THERAPIST</NavLink>
+          </li>
+          <li>
+            <button className="logout_btn" type="button">LOGOUT</button>
+          </li>
+        </nav>
 
-            <figure className="nav_social_icons">
-              <Link to="/" className="td_none">
-                <img src={facebookImg} alt="" />
-              </Link>
-            </figure>
+        <footer className="home_nav_footer">
 
-            <figure className="nav_social_icons">
-              <Link to="/" className="td_none">
-                <img src={twitterImg} alt="" />
-              </Link>
-            </figure>
+          <figure className="nav_social_icons">
+            <Link to="/" className="td_none">
+              <img src={facebookImg} alt="" />
+            </Link>
+          </figure>
 
-            <figure className="nav_social_icons">
-              <Link to="/" className="td_none">
-                <img src={linkedinImg} alt="" />
-              </Link>
-            </figure>
+          <figure className="nav_social_icons">
+            <Link to="/" className="td_none">
+              <img src={twitterImg} alt="" />
+            </Link>
+          </figure>
 
-            <figure className="nav_social_icons">
-              <Link to="/" className="td_none">
-                <img src={instagramImg} alt="" />
-              </Link>
-            </figure>
+          <figure className="nav_social_icons">
+            <Link to="/" className="td_none">
+              <img src={linkedinImg} alt="" />
+            </Link>
+          </figure>
 
-            <figure className="nav_social_icons">
-              <Link to="/" className="td_none">
-                <img src={snapchatImg} alt="" />
-              </Link>
-            </figure>
+          <figure className="nav_social_icons">
+            <Link to="/" className="td_none">
+              <img src={instagramImg} alt="" />
+            </Link>
+          </figure>
 
-          </footer>
-        )}
+          <figure className="nav_social_icons">
+            <Link to="/" className="td_none">
+              <img src={snapchatImg} alt="" />
+            </Link>
+          </figure>
+
+        </footer>
 
       </section>
 
