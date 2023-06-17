c2f6446e23a659bd65040e5b033b2ad6904e1eb7
Choice Osobor <choiceosobor@gmail.com>
2023-06-17 17:32:09 +0100

    Use axios to make a request

--- src/redux/Reserve/reserveSlice.js
+++ src/redux/Reserve/reserveSlice.js
@@ -31,3 +31,24 @@ export const fetchReserve = createAsyncThunk('reserve/fetchReserve', async ({ ge
     return error;
   }
 });
+
+export const postReserve = createAsyncThunk('reserve/postReserve', async (reserveData, { getState }) => {
+    try {
+      const { token } = getState().user;
+      const config = {
+        method: 'post',
+        url: postReserveURL,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        data: reserveData,
+      };
+      const response = await axios(config);
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  });
