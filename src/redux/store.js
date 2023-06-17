6aef176c283f18fce590f87c705717b8dccab320
Choice Osobor <choiceosobor@gmail.com>
2023-06-17 17:36:29 +0100

    Set the Redux store with the reducers for the therapy, user, and reserve slices and manage the state for these slices using Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
+Store.js
+
 import { configureStore } from '@reduxjs/toolkit';
 import therapyReducer from './Therapy/therapySlice';
-
+import userReducer from './User/userSlice';
+import reserveReducer from './Reserve/reserveSlice';
 
 const store = configureStore({
   reducer: {
     therapy: therapyReducer,
-    
+    user: userReducer,
+    reserve: reserveReducer,
   },
 });
 
