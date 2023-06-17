d224e7f7aa703dec13964eed36d9e895d6234bc7
Choice Osobor <choiceosobor@gmail.com>
2023-06-17 17:27:04 +0100

    Use fetchReserve action to send a Get request to getReserveURL

--- src/redux/Reserve/reserveSlice.js
+++ src/redux/Reserve/reserveSlice.js
@@ -1,2 +1,33 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+
+const getReserveURL = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=24';
+const postReserveURL = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=24';
+const deleteReserveURL = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=24';
+
+const initialState = {
+  loading: false,
+  reserves: [],
+  error: '',
+};
+
+export const fetchReserve = createAsyncThunk('reserve/fetchReserve', async ({ getState }) => {
+  try {
+    const { token } = getState().user;
+    const config = {
+      method: 'get',
+      url: getReserveURL,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await axios(config);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+});
