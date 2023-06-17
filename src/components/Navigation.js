4d1d5f7a3ceb91f36c353dbcc6fd2667fbab23c3
Choice Osobor <choiceosobor@gmail.com>
2023-06-17 14:47:52 +0100

    Create nav function and add homepage

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,3 +2,19 @@ import React, { useState } from 'react';
 import '../css/Navigation.css';
 import { Link, NavLink } from 'react-router-dom';
 import '../css/LoginPage.css';
+
+
+function Navigation() {
+    const [isToggled, setIsToggled] = useState(true);
+
+    return (
+    <main>
+        <section className="home_nav_section">
+
+        <header className="home_logo">
+            <figure>
+            <Link to="/homepage" className="td_none">
+                <img src={logoImg} alt="" />
+            </Link>
+            </figure>
+        </header>
