eea911ec50cd0ff2b83384106da93c8b348ef448
Choice Osobor <choiceosobor@gmail.com>
2023-06-17 17:15:06 +0100

    Add more therapist details to page

--- src/components/therapist.js
+++ src/components/therapist.js
@@ -46,3 +46,33 @@ function Therapists() {
                   </Link>
                 </figure>
               </div>
+
+              <div className="home_page_socials">
+              <figure className="home_page_social_icons">
+                <button type="button">
+                  <img src={trashImg} alt="" />
+                </button>
+              </figure>
+            </div>
+
+          </article>
+        </Link>
+      </div>
+
+      <div className="home_grids_box">
+        <Link className="td_none" to="/therapistdetails">
+          <article>
+            <figure className="therapy_figure">
+              <img src={therapistImg} alt="" />
+            </figure>
+
+            <h4>MARCUS DAVID</h4>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, id.</p>
+
+            <div className="home_page_socials">
+              <figure className="home_page_social_icons">
+                <Link to="/" className="td_none">
+                  <img src={facebookImg} alt="" />
+                </Link>
+              </figure>
