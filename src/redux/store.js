42bae2c5b86489d788be7d58a598275026e4ae0b
Choice Osobor <choiceosobor@gmail.com>
2023-06-17 13:19:44 +0100

    modify files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import therapyReducer from './Therapy/therapySlice';
-import userReducer from './User/userSlice';
-import reserveReducer from './Reserve/reserveSlice';
+
 
 const store = configureStore({
   reducer: {
