3178989f2872618f00b5c070cf98f381d0d5426e
Choice Osobor <choiceosobor@gmail.com>
2023-06-20 09:52:55 +0100

    Add and modify test case for redux userSlice

--- src/__test__/reduce/userslice.test.js
+++ src/__test__/reduce/userslice.test.js
@@ -1,24 +1,79 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import axios from 'axios';
-import store from '../../redux/store';
-import { testFetchMarkets } from '../../redux/Market/marketSlice';
+import {
+  registerUser,
+  loginUser,
+  forgotPassword,
+  logoutUser,
+} from '../../redux/User/userSlice';
 
-describe('Market redux state tests', () => {
-  it('Should initially set Market store to an empty Array', () => {
-    const state = store.getState().market.markets;
-    expect(state).toEqual([]);
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+jest.mock('axios');
+
+describe('User redux state tests', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      user: {
+        loading: false,
+        user: [],
+        error: '',
+      },
+    });
   });
 
-  it('fetches market data from API', async () => {
-    const url = 'https://api.coinstats.app/public/v1/markets?coinId=bitcoin';
-    const axiosSpy = jest.spyOn(axios, 'get');
-    jest.setTimeout(90000);
-    const dispatchSpy = jest.fn();
-    await testFetchMarkets(url)(dispatchSpy);
-    expect(axiosSpy).toHaveBeenCalledWith(url);
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('registers a user successfully', async () => {
+    const registerData = { /* provide registration data */ };
+    const responseData = { /* provide response data */ };
+    axios.post.mockResolvedValue({ data: responseData });
+
+    await store.dispatch(registerUser(registerData));
+
+    const actions = store.getActions();
+    expect(actions[0].type).toBe('user/registerUser/pending');
+    expect(actions[1].type).toBe('user/registerUser/fulfilled');
+  });
+
+  it('logs in a user successfully', async () => {
+    const loginData = { /* provide login data */ };
+    const responseData = { /* provide response data */ };
+    axios.post.mockResolvedValue({ data: responseData });
+
+    await store.dispatch(loginUser(loginData));
+
+    const actions = store.getActions();
+    expect(actions[0].type).toBe('user/loginUser/pending');
+    expect(actions[1].type).toBe('user/loginUser/fulfilled');
+  });
+
+  it('sends a forgot password request successfully', async () => {
+    const userData = { /* provide user data */ };
+    const responseData = { /* provide response data */ };
+    axios.put.mockResolvedValue({ data: responseData });
+
+    await store.dispatch(forgotPassword(userData));
+
+    const actions = store.getActions();
+    expect(actions[0].type).toBe('user/forgotPassword/pending');
+    expect(actions[1].type).toBe('user/forgotPassword/fulfilled');
+  });
+
+  it('logs out a user successfully', async () => {
+    const responseData = { /* provide response data */ };
+    axios.delete.mockResolvedValue({ data: responseData });
+
+    await store.dispatch(logoutUser());
+
+    const actions = store.getActions();
+    expect(actions[0].type).toBe('user/logoutUser/pending');
+    expect(actions[1].type).toBe('user/logoutUser/fulfilled');
   });
 });
-
-// export const testFetchCharts = createAsyncThunk('market/fetchMarkets', async () => {
-//   const { data } = await (axios.get('https://api.coinstats.app/public/v1/charts?period=1m&coinId=bitcoin'));
-//   return data;
-// });
