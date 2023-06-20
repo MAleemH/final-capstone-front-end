0925cd87814be0ceb721b860b63e1c13de6cc8ba
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-06-20 08:17:05 -0700

    Fix serializable

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import therapyReducer from './Therapy/therapySlice';
 import userReducer from './User/userSlice';
 import reserveReducer from './Reserve/reserveSlice';
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+});
 
 const store = configureStore({
   reducer: {
@@ -9,6 +13,7 @@ const store = configureStore({
     user: userReducer,
     reserve: reserveReducer,
   },
+  middleware: customizedMiddleware,
 });
 
 export default store;
