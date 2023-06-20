76f42edf89d3da0b5313f47769feca0224de288b
Choice Osobor <choiceosobor@gmail.com>
2023-06-20 09:16:47 +0100

    Installed essential dependencies and add test case for reserveSlice

--- src/__test__/reduce/reserveslice.test.js
+++ src/__test__/reduce/reserveslice.test.js
@@ -1,19 +1,60 @@
-import axios from 'axios';
-import store from '../../redux/store';
-import { fetchCharts } from '../../redux/Chart/chartSlice';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {
+  fetchReserves,
+  fetchSingleReserve,
+  postReserve,
+  deleteReserve,
+} from '../../redux/Reserve/reserveSlice';
 
-describe('Chart redux state tests', () => {
-  it('Should initially set Chart store to an empty Array', () => {
-    const state = store.getState().chart.charts;
-    expect(state).toEqual([]);
+const mockStore = configureStore([thunk]);
+
+describe('reserveSlice', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      reserve: {
+        loading: false,
+        reserves: [],
+        singleReserve: null,
+        error: '',
+      },
+    });
   });
 
-  it('fetches coin data from API', async () => {
-    const url = 'https://api.coinstats.app/public/v1/charts?period=1m&coinId=bitcoin';
-    const axiosSpy = jest.spyOn(axios, 'get');
-    jest.setTimeout(90000);
-    const dispatchSpy = jest.fn();
-    await testFetchCharts(url)(dispatchSpy);
-    expect(axiosSpy).toHaveBeenCalledWith(url);
+  it('should fetch reserves', async () => {
+    await store.dispatch(fetchReserves());
+
+    const actions = store.getActions();
+    expect(actions[0].type).toEqual(fetchReserves.pending.type);
+    expect(actions[1].type).toEqual(fetchReserves.fulfilled.type);
+  });
+
+  it('should fetch a single reserve', async () => {
+    const reserveID = expect.any(Number);
+    await store.dispatch(fetchSingleReserve(reserveID));
+
+    const actions = store.getActions();
+    expect(actions[0].type).toEqual(fetchSingleReserve.pending.type);
+    expect(actions[1].type).toEqual(fetchSingleReserve.fulfilled.type);
+  });
+
+  it('should post a reserve', async () => {
+    const reserveData = expect.any(Object);
+    await store.dispatch(postReserve(reserveData));
+
+    const actions = store.getActions();
+    expect(actions[0].type).toEqual(postReserve.pending.type);
+    expect(actions[1].type).toEqual(postReserve.fulfilled.type);
+  });
+
+  it('should delete a reserve', async () => {
+    const deleteID = expect.any(Number);
+    await store.dispatch(deleteReserve(deleteID));
+
+    const actions = store.getActions();
+    expect(actions[0].type).toEqual(deleteReserve.pending.type);
+    expect(actions[1].type).toEqual(deleteReserve.fulfilled.type);
   });
 });
